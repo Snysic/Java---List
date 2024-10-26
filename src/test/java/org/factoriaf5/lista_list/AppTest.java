9139c7f4a0cc899a59793897aa875cb58f933010
Maria Vasilenko <snysic1987@gmail.com>
2024-10-26 21:23:32 +0200

    test for createList

--- src/test/java/org/factoriaf5/lista_list/AppTest.java
+++ src/test/java/org/factoriaf5/lista_list/AppTest.java
@@ -1,6 +1,21 @@
 package org.factoriaf5.lista_list;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
 
+class HandlerWeekDaysTest {
+    private HandlerWeekDays handler;
+
+    @BeforeEach
+    void setUp() {
+        handler = new HandlerWeekDays();
+        handler.createList();
+    }
+
+    @Test
+    void testCreateList() {
+        assertEquals(7, handler.getSizeList());
+    }
+
+}
